18eed068b4206fd51964ff6a9bb964687037c90c
sumanjeet0012 <sumanjeet0012@gmail.com>
2023-12-30 18:50:51 +0530

    upper sidebar created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,45 @@ import "./App.css";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="App">
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src="/assets/chatgpt.svg" alt="Logo" />
+            <span className="brand">Chat GPT</span>
+          </div>
+          <button className="midBtn">
+            <img src="/assets/add-30.png" alt="" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideButton">
+            <button className="query">
+              <img src="/assets/message.svg" alt="query" />
+              What is Programming
+            </button>
+            <button className="query">
+              <img src="/assets/message.svg" alt="query" />
+              How to use an API
+            </button>
+          </div>
+        </div>
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src="/assets/home.svg" alt="" />
+            Home
+          </div>
+          <div className="listItems">
+            <img src="/assets/bookmark.svg" alt="" />
+            Saved
+          </div>
+          <div className="listItems">
+            <img src="/assets/rocket.svg" alt="" />
+            Upgrade to Pro
+          </div>
+        </div>
+      </div>
+      <div className="main"></div>
+    </div>
   );
 }
 
